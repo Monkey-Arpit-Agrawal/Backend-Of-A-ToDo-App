8b4ce4f045774306f3d7510975c831f786952c53
Arpit Agrawal <agrawalarpit456@gmail.com>
2025-07-14 14:07:23 +0530

    Progress Done

---
+++ models/easy.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose" ;
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const User = new Schema({
+  username: String,
+  email: String,
+  password: String
+});
+
+const Todo = new Schema({
+    userId: ObjectId,
+    title: String,
+    done: Boolean
+});
+
+const UserModel = mongoose.model('users', User);
+const TodoModel = mongoose.model('todos', Todo);
+
+export const modules = {
+    UserModel,
+    TodoModel
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import dotenv from 'dotenv' ;
 import express from 'express' ;
 import { User } from '../models/user.models.js' ;
 import { Todo } from '../models/todo.models.js' ;
+import { modules } from '../models/easy.js'
+import mongoose from 'mongoose' ;
 
+mongoose.connect('mongodb://localhost:27017/todos')
 
 
 dotenv.config() ;
@@ -13,13 +16,19 @@ const port = process.env.PORT || 3000 ;
 
 app.use(express.json()) ;
 
-app.post('/signup' , function (req,res){
+app.post('/signup' , async function (req,res){
 
     let username = req.body.username ;
     let password = req.body.password ;
     let email = req.body.email ;
 
-    
+    await modules.UserModel.create({
+        email: email,
+        password: password,
+        username: username
+    });
+
+    res.status(200).send('Signup Success') ;
 
 }) ;
 
